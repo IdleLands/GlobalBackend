ad4fd7f253ec10e39bffc75310a5673b7ddff803
Kain <drkain@outlook.com>
2020-02-28 01:50:07 +1300

    Get resources and crystals in guild

--- src/routes/guild.js
+++ src/routes/guild.js
@@ -3,14 +3,26 @@ const { DB } = require('../db');
 
 exports.route = (app) => {
   app.get('/guild', async (req, res) => {
-    
+
     const guild = await DB.$guilds
-    .findOne(
-      { name: req.query.name },
-      { fields: { motd: 1, recruitment: 1, taxes: 1, name: 1, tag: 1, buildingLevels: 1, members: 1 } }
-    );
+      .findOne(
+        { name: req.query.name },
+        {
+          fields: {
+            motd: 1,
+            recruitment: 1,
+            taxes: 1,
+            name: 1,
+            tag: 1,
+            buildingLevels: 1,
+            members: 1,
+            resources: 1,
+            crystals: 1
+          }
+        }
+      );
 
     res.json(guild);
-    
+
   });
 }
